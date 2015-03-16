94e30cd6aaa2b948510574e62da897690d1c857d
flowyears <flowyears1225@gmail.com>
2015-03-16 17:41:08 +0800

    Initial Commit

---
+++ KVO_KVCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KVO_KVCDemo
+//
+//  Created by 沈佳雄 on 15/3/16.
+//  Copyright (c) 2015年 shenjx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KVO_KVCDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  KVO_KVCDemo
+//
+//  Created by 沈佳雄 on 15/3/16.
+//  Copyright (c) 2015年 shenjx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
